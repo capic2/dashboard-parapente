24a2be1871c63d7a18dde69096b9eecffa97d4da
Vincent CAPICOTTO <vcapic@gmail.com>
2026-05-07 18:55:21 +0200

    add skill ui/ux

---
+++ apps/frontend/src/components/auth/LoginForm.stories.tsx
@@ -0,0 +1,23 @@
+import preview from '../../../.storybook/preview.tsx';
+import { LoginForm } from './LoginForm.tsx';
+import { fn } from 'storybook/test';
+
+const meta = preview.meta({
+  title: 'Components/Common/AppUpdateBanner/Chromatic',
+  component: LoginForm,
+  parameters: {
+    layout: 'padded',
+    chromatic: {
+      disableSnapshot: false,
+    },
+  },
+  tags: ['!autodocs'],
+});
+
+export const Default = meta.story({
+  name: 'Default',
+  args: {
+    onSubmit: fn(),
+    isPending: false,
+  },
+});
